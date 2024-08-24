ac0cef555232a2d5781200d50c63fb6cf865c0b6
AbYahaya <yabdulrauf911@gmail.com>
2024-08-24 08:59:19 -0700

    Changes entire content

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,34 +1,10 @@
-# Ensure Nginx is installed
-package { 'nginx':
-  ensure => installed,
+# Fix the nginx requests limit
+exec { 'upgrade':
+  path    => '/bin/',
+  command => 'sed -i "s/15/4096" /etc/default/nginx',
 }
 
-# Ensure Nginx service is enabled and running
-service { 'nginx':
-  ensure    => running,
-  enable    => true,
-  hasstatus => true,
-  hasrestart => true,
-  require   => Package['nginx'],
-}
-
-# Ensure Apache is not running (if you want to disable Apache)
-service { 'apache2':
-  ensure => stopped,
-  enable => false,
-  require => Package['apache2'],
-}
-
-# Basic Nginx configuration (if needed)
-file { '/etc/nginx/sites-available/default':
-  ensure  => file,
-  content => template('nginx/default.erb'),
-  require => Package['nginx'],
-  notify  => Service['nginx'],
-}
-
-# Enable Nginx site
-exec { 'enable_nginx_default_site':
-  command => '/usr/sbin/nginx -s reload',
-  require => File['/etc/nginx/sites-available/default'],
+exec { 'restart':
+  path    => '/usr/bin/',
+  command => 'service nginx restart',
 }
